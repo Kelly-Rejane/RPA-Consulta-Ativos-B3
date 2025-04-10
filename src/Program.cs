189ffd549b03225121424a1f828c0ac1d29dc98f
Kelly Ferreira <kelly.estudos23@gmail.com>
2025-04-10 18:23:51 -0300

    Configuração de Logger Personalizado

--- src/Program.cs
+++ src/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using RpaConsultaAtivosB3.src.Utils;
+
+//Configuração do logger
+// Essa configuração é feita antes de criar o Host, para que o logger seja utilizado em toda a aplicação.
+Logger.CreateLogger();
+
+// Criação do Host
+// O Host é responsável por gerenciar a aplicação, incluindo a configuração de serviços e o 
+//ciclo de vida da aplicação.
+var builder = Host.CreateDefaultBuilder(args)
+    .UseSerilog() // usa Serilog como logger global
+    .ConfigureServices((context, services) =>
+    {
+        //configuração dos serviços
+    });
+
+var app = builder.Build();
+
+//Execução aqui
+
+app.Run();
--- src/Utils/Logger.cs
+++ src/Utils/Logger.cs
@@ -2,11 +2,32 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Sinks.SystemConsole.Themes;
 
 namespace RpaConsultaAtivosB3.src.Utils
 {
-    public class Logger
+    // Essa classe é responsável por criar o logger que será utilizado em toda a aplicação.
+    // Ela utiliza a biblioteca Serilog para criar logs em console e em arquivo.
+    public class Logger 
     {
-        
+        public static void CreateLogger()
+        {
+            var logPath = Path.Combine(AppContext.BaseDirectory, "Logs", "logs", "log.txt");
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .Enrich.FromLogContext()
+                .WriteTo.Console(
+                    theme: AnsiConsoleTheme.Code,
+                    outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}")
+                .WriteTo.File(
+                    path: logPath,
+                    rollingInterval: RollingInterval.Day,
+                    retainedFileCountLimit: 7, // mantém os últimos 7 dias
+                    outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}")
+                .CreateLogger();
+        }
     }
 }
